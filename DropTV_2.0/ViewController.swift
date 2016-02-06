2bb9bd0f71202a8edfd96748da96dfb3077426c8
林凱君 <kelly08385@gmail.com>
2016-02-06 16:55:33 +0800

    Initial Commit

---
+++ DropTV_2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DropTV_2.0
+//
+//  Created by tw itri on 2016/2/6.
+//  Copyright © 2016年 tw_itri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
